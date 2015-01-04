9db859523d573607c879cb1c3382c62465d7a4c4
Brian Dupras <brian@duprasville.com>
2015-01-03 20:05:11 -0700

    no java 1.6

--- plugins/java/java.plugin.zsh
+++ plugins/java/java.plugin.zsh
@@ -1,4 +1,4 @@
-export JAVA6_HOME=$(/usr/libexec/java_home -v 1.6)
+#export JAVA6_HOME=$(/usr/libexec/java_home -v 1.6)
 export JAVA7_HOME=$(/usr/libexec/java_home -v 1.7)
 export JAVA8_HOME=$(/usr/libexec/java_home -v 1.8)
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
